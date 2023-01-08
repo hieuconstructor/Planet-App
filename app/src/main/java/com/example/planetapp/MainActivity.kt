c36227092f8a5063a5c200693d6539ba8620d1e6
Nguyễn Viết Hiếu <hieuconstructor@gmail.com>
2023-01-09 01:07:45 +0700

    Complete App

--- app/src/main/java/com/example/planetapp/MainActivity.kt
+++ app/src/main/java/com/example/planetapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.planetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.planetapp.adapter.Adapter
+import com.example.planetapp.data.DataSource
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.apply {
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        my_recycler.adapter = Adapter(DataSource.SetPlanets())
+        my_recycler.layoutManager = LinearLayoutManager(this)
     }
 }
